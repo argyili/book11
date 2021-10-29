e3b6608a89478017a9425f63aaf0e9128a46d654
argyi <argyi.liaiyi@gmail.com>
2021-10-29 17:10:22 +0900

    Add traversal method for graph.

---
+++ graph/dfs.java
@@ -0,0 +1,62 @@
+// DFS algorithm in Java
+// DFS(G, u)
+//     u.visited = true
+//     for each v ∈ G.Adj[u]
+//         if v.visited == false
+//             DFS(G,v)
+     
+// init() {
+//     For each u ∈ G
+//         u.visited = false
+//      For each u ∈ G
+//        DFS(G, u)
+// }
+
+import java.util.*;
+
+class Graph {
+  private LinkedList<Integer> adjLists[];
+  private boolean visited[];
+
+  // Graph creation
+  Graph(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
+  }
+
+  // Add edges
+  void addEdge(int src, int dest) {
+    adjLists[src].add(dest);
+    // adjLists[dest].add(src);
+  }
+
+  // DFS algorithm
+  void DFS(int vertex) {
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+
+    Iterator<Integer> ite = adjLists[vertex].listIterator();
+    while (ite.hasNext()) {
+      int adj = ite.next();
+      if (!visited[adj])
+        DFS(adj);
+    }
+  }
+
+  public static void main(String args[]) {
+    Graph g = new Graph(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+    System.out.println("Following is Depth First Traversal");
+
+    g.DFS(2);
+}
+}
