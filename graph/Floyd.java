937d3f1231878675066eb300aec04e29e73f091b
argyi <argyi.liaiyi@gmail.com>
2021-11-16 17:50:43 +0900

    Upload two ways to find a shortest path

---
+++ graph/Floyd.java
@@ -0,0 +1,45 @@
+// Floyd Warshall Algorithm in Java
+
+class Floyd {
+  final static int INF = 9999, nV = 4;
+
+  // Implementing floyd warshall algorithm
+  void floydWarshall(int graph[][]) {
+    int matrix[][] = new int[nV][nV];
+    // i means start point, j means destination, k means count
+    int i, j, k;
+
+    for (i = 0; i < nV; i++)
+      for (j = 0; j < nV; j++)
+        matrix[i][j] = graph[i][j];
+
+    // Adding vertices individually
+    for (k = 0; k < nV; k++) {
+      for (i = 0; i < nV; i++) {
+        for (j = 0; j < nV; j++) {
+          if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+            matrix[i][j] = matrix[i][k] + matrix[k][j];
+        }
+      }
+    }
+    printMatrix(matrix);
+  }
+
+  void printMatrix(int matrix[][]) {
+    for (int i = 0; i < nV; ++i) {
+      for (int j = 0; j < nV; ++j) {
+        if (matrix[i][j] == INF)
+          System.out.print("INF ");
+        else
+          System.out.print(matrix[i][j] + "  ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int graph[][] = { { 0, 3, INF, 5 }, { 2, 0, INF, 4 }, { INF, 1, 0, INF }, { INF, INF, 2, 0 } };
+    Floyd a = new Floyd();
+    a.floydWarshall(graph);
+  }
+}
